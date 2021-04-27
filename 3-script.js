9f039873f5c68a42722984f74deb6deebfe45314
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-04-27 18:54:57 -0500

    Update 3-script.js

--- 3-script.js
+++ 3-script.js
@@ -4,7 +4,7 @@ console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 console.log(23);
 
-let firstName = "Jonas";
+let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
