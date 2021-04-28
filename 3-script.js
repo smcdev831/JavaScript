f585d8afcd452e390bce5d5f57828938c6d1901b
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-04-27 19:05:14 -0500

    Update 3-script.js

--- 3-script.js
+++ 3-script.js
@@ -16,8 +16,10 @@ let person = "jonas"
 
 let PI = 3.1415;
 
-let myFirstJob = "Programmer";
+let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
 
 let job1 = "programmer";
 let job2 = "teacher";
+
+console.log(myFirstJob);
