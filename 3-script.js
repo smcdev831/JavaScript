88e4a87218eef4ce7d98a6ecdce86ca2caba15eb
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-04-27 19:04:07 -0500

    Update 3-script.js

--- 3-script.js
+++ 3-script.js
@@ -8,9 +8,16 @@ let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+
+let jonas_matilda = "JM"
+let $function = 27;
+
+let person = "jonas"
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
