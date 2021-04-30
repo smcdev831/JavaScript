d796b751c894db3f045df36d0f030272b39f6c11
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-04-29 19:29:35 -0500

    session 4

--- 4-script.js
+++ 4-script.js
@@ -24,3 +24,11 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
