73ba6a795f31ec266a58af5a8021af2850d9a865
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-04-30 19:36:54 -0500

    Update 4-script.js

--- 4-script.js
+++ 4-script.js
@@ -35,3 +35,12 @@ console.log(typeof javascriptIsFun);
 
 javascriptIsFun = "YES!";
 console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
