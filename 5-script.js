16cc5b98bdd644600ff877e4fbdb11fd17c4a5b0
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-05-02 22:24:43 -0500

    Update 5-script.js

--- 5-script.js
+++ 5-script.js
@@ -44,7 +44,7 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
+
 
 let age = 30;
 age = 31;
@@ -56,3 +56,7 @@ const birthYear = 1991;
 
 var job = "Programmer";
 job = "Teacher"
+*/
+
+const ageJonas = 2037 - 1991;
+console.log(ageJonas);
