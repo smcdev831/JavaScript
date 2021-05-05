fe3f39d8775e44531e0ace07cb7af18dd28573f0
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-05-04 19:22:30 -0500

    Update 5-script.js

--- 5-script.js
+++ 5-script.js
@@ -58,5 +58,25 @@ var job = "Programmer";
 job = "Teacher"
 */
 
-const ageJonas = 2037 - 1991;
-console.log(ageJonas);
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas*2, ageJonas/10, 2**3);
+
+// 2**3 means 2 to the 3rd power or 2*2*2
+
+const firstName = "Jonas"
+const lastName = "Schmedtmann"
+console.log(firstName + " " + lastName);
+
+let x = 10+5; //15
+x += 10; //25
+x *= 4; //100
+x++;
+x--;
+x--;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
