3bd1c2cb701784ccc3e978e3e8106df090ff42a9
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-05-04 19:23:47 -0500

    Session 6

---
+++ 6-script.js
@@ -0,0 +1,82 @@
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//variable name conventions
+let jonas_matilda = "JM"
+let $function = 27;
+
+let person = "jonas"
+
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "Programmer";
+job = "Teacher"
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas*2, ageJonas/10, 2**3);
+
+// 2**3 means 2 to the 3rd power or 2*2*2
+
+const firstName = "Jonas"
+const lastName = "Schmedtmann"
+console.log(firstName + " " + lastName);
+
+let x = 10+5; //15
+x += 10; //25
+x *= 4; //100
+x++;
+x--;
+x--;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+*/
