8a47d2fcc7c7946f6e436464b56e1f10d79f4cd7
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-05-10 19:26:42 -0500

    Update CodingChallenge-2.js

--- CodingChallenge-2.js
+++ CodingChallenge-2.js
@@ -53,7 +53,7 @@ let johnInfoh = 1.76;
 let johnBMI=(johnInfom / (johnInfoh * johnInfoh));
 
 if(johnBMI > markBMI) {
-  console.log(`John's BMI is higher`);
+  console.log(`John's BMI is higher than Mark's`);
 } else {
-  console.log(`Mark's BMI is higher`);
+  console.log(`Mark's BMI is higher than John's`);
 }
