3e93c03fff226061fc062e4b384b246307e2f53b
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-06-01 21:40:05 -0500

    Update 9-script.js

--- 9-script.js
+++ 9-script.js
@@ -211,3 +211,28 @@ if(hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log(`Let someone else drive.`);
 }*/
+
+let day = "monday"
+
+switch (day) {
+  case 'monday':
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend!');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
